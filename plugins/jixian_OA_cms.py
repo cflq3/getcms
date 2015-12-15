6cf07da0ea3ec644866df715cff1f311a46ee378
hackerlq <hackerlq@gmail.com>
2015-12-16 02:13:07 +0800

    add plugins

---
+++ plugins/jixian_OA_cms.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+def run(whatweb, pluginname):
+    whatweb.recog_from_file(pluginname, "templates/2010_01/index.css", "submit.jpg")
+
---
+++ plugins/seeyon_A8_cms.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+def run(whatweb, pluginname):
+    whatweb.recog_from_file(pluginname, "seeyon/common/js/V3X.js", "seeyon")
+
